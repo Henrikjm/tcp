882bb02cf3be1a91d01fcd295b8024bcdb61cdfd
Henrikjm <melandhenrik@gmail.com>
2015-06-30 11:52:23 +0200

    Changed port

--- server_tcp.py
+++ server_tcp.py
@@ -3,15 +3,22 @@ import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 12332
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
+print '1'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print '2'
 s.bind((TCP_IP, TCP_PORT))
+print '3'
 s.listen(1)
+print '4'
 conn, addr = s.accept()
+print '5'
 print 'Connection address:', addr
+print '6'
 
 while 1:
+  print '7'
   data = conn.recv(BUFFER_SIZE)
   if not data: break
   print "received data:", data
