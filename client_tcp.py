05a2109e65408b08c5afa113fc87670e4cb7f7c0
Henrikjm <melandhenrik@gmail.com>
2015-06-30 11:44:24 +0200

    Fixed indention errors

--- client_tcp.py
+++ client_tcp.py
@@ -1,17 +1,17 @@
- #!/usr/bin/env python
-   2 
-   3 import socket
-   4 
-   5 
-   6 TCP_IP = '127.0.0.1'
-   7 TCP_PORT = 5005
-   8 BUFFER_SIZE = 1024
-   9 MESSAGE = "Hello, World!"
-  10 
-  11 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  12 s.connect((TCP_IP, TCP_PORT))
-  13 s.send(MESSAGE)
-  14 data = s.recv(BUFFER_SIZE)
-  15 s.close()
-  16 
-  17 print "received data:", data
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
