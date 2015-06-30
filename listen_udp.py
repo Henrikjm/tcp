dde43f8abaf9e91e83edc909d4cf226a3a409f6d
Henrikjm <melandhenrik@gmail.com>
2015-06-30 13:46:09 +0200

    changed udp implementation

--- listen_udp.py
+++ listen_udp.py
@@ -1,13 +1,45 @@
-
+'''
+    Simple udp socket server
+    Silver Moon (m00n.silv3r@gmail.com)
+'''
+ 
 import socket
-
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
-
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+import sys
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+# Datagram (udp) socket
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+ 
+ 
+# Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#now keep talking with the client
+while 1:
+    # receive data from client (data, addr)
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
+     
+    if not data: 
+        break
+     
+    reply = 'OK...' + data
+     
+    s.sendto(reply , addr)
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+     
+s.close()
--- send_udp.py
+++ send_udp.py
@@ -1,13 +1,35 @@
-import socket
-
-UDP_IP = "78.91.34.147"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
-
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+'''
+    udp socket client
+    Silver Moon
+'''
+ 
+import socket   #for sockets
+import sys  #for exit
+ 
+# create dgram udp socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+ 
+host = 'localhost';
+port = 8888;
+ 
+while(1) :
+    msg = raw_input('Enter message to send : ')
+     
+    try :
+        #Set the whole string
+        s.sendto(msg, (host, port))
+         
+        # receive data from client (data, addr)
+        d = s.recvfrom(1024)
+        reply = d[0]
+        addr = d[1]
+         
+        print 'Server reply : ' + reply
+     
+    except socket.error, msg:
+        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
